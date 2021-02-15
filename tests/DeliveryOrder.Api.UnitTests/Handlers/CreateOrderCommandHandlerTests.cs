37804171dbea20269c3df9a9de12806f4d484577
IqbalN <iqbal.nirbito+atlassian@gmail.com>
2021-02-15 19:56:50 +0700

    Handlers and validator unit tests.

---
+++ tests/DeliveryOrder.Api.UnitTests/Handlers/CreateOrderCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿// -------------------------------------------------------------
+// Copyright Go-Logs. All rights reserved.
+// Proprietary and confidential.
+// Unauthorized copying of this file is strictly prohibited.
+// -------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using GoLogs.Framework.Mvc;
+using GoLogs.Services.DeliveryOrder.Api.Application.Internals;
+using GoLogs.Services.DeliveryOrder.Api.Commands;
+using Moq;
+using Xunit;
+
+namespace GoLogs.Services.DeliveryOrder.Api.UnitTests.Handlers
+{
+    public class CreateOrderCommandHandlerTests
+    {
+        private readonly Mock<IDOOrderContext> _mockContext;
+        private readonly Mock<IProblemCollector> _mockProblemCollector;
+
+        public CreateOrderCommandHandlerTests()
+        {
+            _mockContext = new Mock<IDOOrderContext>();
+            _mockProblemCollector = new Mock<IProblemCollector>();
+        }
+
+        [Fact]
+        public async Task Handle_EmptyCommand_ThrowsException()
+        {
+            var handler = new CreateOrderCommandHandler(_mockContext.Object, _mockProblemCollector.Object);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => handler.Handle(null));
+        }
+    }
+}
---
+++ tests/DeliveryOrder.Api.UnitTests/Handlers/DOOrderCreatedEventHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------
+// Copyright Go-Logs. All rights reserved.
+// Proprietary and confidential.
+// Unauthorized copying of this file is strictly prohibited.
+// -------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using GoLogs.Services.DeliveryOrder.Api.Infrastructure.Messaging;
+using MassTransit;
+using Moq;
+using Xunit;
+
+namespace GoLogs.Services.DeliveryOrder.Api.UnitTests.Handlers
+{
+    public class DOOrderCreatedEventHandlerTests
+    {
+        private readonly Mock<IPublishEndpoint> _mockPublishEndpoint;
+
+        public DOOrderCreatedEventHandlerTests()
+        {
+            _mockPublishEndpoint = new Mock<IPublishEndpoint>();
+        }
+
+        [Fact]
+        public async Task Handle_EmptyEvent_ThrowsException()
+        {
+            var handler = new DOOrderCreatedEventHandler(_mockPublishEndpoint.Object);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => handler.Handle(null));
+        }
+    }
+}
