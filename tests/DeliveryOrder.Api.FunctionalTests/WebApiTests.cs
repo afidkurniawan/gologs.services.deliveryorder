3dceef94db15efb73b03e448384299fa8eb32c05
IqbalN <iqbal.nirbito+atlassian@gmail.com>
2021-03-31 21:16:02 +0700

    Added web API tests.

---
+++ tests/DeliveryOrder.Api.FunctionalTests/WebApiTests.cs
@@ -0,0 +1,35 @@
+﻿// -------------------------------------------------------------
+// Copyright Go-Logs. All rights reserved.
+// Proprietary and confidential.
+// Unauthorized copying of this file is strictly prohibited.
+// -------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace GoLogs.Services.DeliveryOrder.Api.FunctionalTests
+{
+    public class WebApiTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public WebApiTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/DOOrders")]
+        [InlineData("/DOOrders?page=1&pageSize=1")]
+        public async Task Get_EndpointReturnsSuccessAndCorrectContentType(string url)
+        {
+            var response = await _client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(response.Content.Headers.ContentType);
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
